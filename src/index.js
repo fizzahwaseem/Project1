b1eafd94b32a7e164bfafa191d5ac18040d7af8c
fizzahwaseem <50530599+fizzahwaseem@users.noreply.github.com>
2020-08-04 17:49:10 +0500

    updatede index.js 'helloo world'

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <div>Helloo world</div>
+  <div>Helloo world</div>,
   document.getElementById('root')
 );
 
